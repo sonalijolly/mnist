af61986052947d41d36e2ee87ab458629dce60a2
sonalijolly <85408006+sonalijolly@users.noreply.github.com>
2021-09-03 14:24:40 +0530

    Update plot_digits_classification.py

--- mnist/plot_digits_classification.py
+++ mnist/plot_digits_classification.py
@@ -27,76 +27,40 @@ for ax, image, label in zip(axes, digits.images, digits.target):
     ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
     ax.set_title('Training: %i' % label)
 
-
 def testf(digits, shape, split):
     n_samples = len(digits.images)
     X,Y = digits.images,digits.target 
-
     sample1 = np.resize(digits.images, (n_samples,shape))
-    
-
     clf = svm.SVC(gamma=0.00001)
-
-
     X_train, X_test, y_train, y_test = train_test_split(
         sample1, digits.target, test_size=split, shuffle=False)
-
-    
-    
-    
     clf.fit(X_train, y_train.reshape(-1,1))
-
-    
     predicted = clf.predict(X_test)
-
-    
     accuracy = metrics.accuracy_score(y_test, predicted)
-    
     return accuracy
 
 shape = 256
-split = 0.75
-a= testf(digits,shape , split)
-print("16*16   ","75:25   ",a)
+split = [0.90,0.70,0.50]
+shapee  = [256,1024]
+for i in split:
+    a= testf(digits,shape , i)
+    print(np.sqrt(shape), "*" , np.sqrt(shape),"--> ",i,":",round(1-i,1),"-->",a)
 
-shape = 256
-split = 0.5
-a= testf(digits,shape , split)
-print("16*16   ","50:50   ", a)
-
-shape = 256
-split = 0.25
-a= testf(digits,shape , split)
-print("16*16   ","25:75   ", a)
 
 
 shape = 1024    
-split = 0.75
-a= testf(digits,shape , split)
-print("32*32  ","75:25   ",a)
-
-shape = 1024    
-split = 0.5
-a= testf(digits,shape , split)
-print("32*32   ","50:50   ", a)
-
-shape = 1024
-split = 0.25
-a= testf(digits,shape , split)
-print("32*32    ","25:75   ", a)
+split = [0.90,0.70,0.50]
+shapee  = [256,1024]
+for i in split:
+    a= testf(digits,shape , i)
+    print(np.sqrt(shape), "*" , np.sqrt(shape),"--> ",i,":",round(1-i,1),"-->",a)
 
 shape = 4096    
-split = 0.75
-a= testf(digits,shape , split)
-print("64*64   ","75:25   ", a)
+split = [0.90,0.70,0.50]
+shapee  = [256,1024]
+for i in split:
+    a= testf(digits,shape , i)
+    print(np.sqrt(shape), "*" , np.sqrt(shape),"--> ",i,":",round(1-i,1),"-->",a)
 
-shape = 4096    
-split = 0.5
-a= testf(digits,shape , split)
-print("64*64   ","75:25   ", a)
 
-shape = 4096
-split = 0.25
-a= testf(digits,shape , split)
-print("64*64   ","75:25   ", a)
 
