ea00f683976d37f240f6ad136a71f50d5f86c7d4
sonalijolly <85408006+sonalijolly@users.noreply.github.com>
2021-11-18 22:33:55 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,24 @@
 from flask import Flask
+from flask import request
+#from mnist.utils import load
+from joblib import dump, load
+import numpy as np
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+best_model_path = "/home/sonali/MLOPs/Mnist/mnist/models/s_8_tt_0.25_val_0.25_gamma0.01/model.joblib"
+
+@app.route("/predict", methods = ['POST'])
+def predict():
+    clf = load(best_model_path)
+    input_json = request.json
+    image = input_json['image']
+    #print(image)
+    #image = np.array(image)
+    image = np.array(image).reshape(1,-1)
+    predicted = clf.predict(image)
+    return str(predicted[0])
