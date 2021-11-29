cc8c43ff4f5ca672ade07963f28651c4750ab0f8
sonalijolly <85408006+sonalijolly@users.noreply.github.com>
2021-11-29 20:41:35 +0530

    Update dockerfile.sh

--- docker/dockerfile.sh
+++ docker/dockerfile.sh
@@ -1,7 +1,6 @@
 FROM ubuntu:18.04
 copy mnist /exp/mnist
-copy requirements.txt /exp/requirements.txt
 RUN apt-get update && apt-get install -y python3 python3-pip
-RUN pip3 install --no-cache-dir -r /exp/requirements.txt
+RUN pip3 install --no-cache-dir -r /exp/mnist/requirements.txt
 WORKDIR /exp
 CMD ["python3", "./mnist/api/hello.py"]
